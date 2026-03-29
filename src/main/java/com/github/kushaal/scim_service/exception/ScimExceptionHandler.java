65b3e99acef82271d7849c42ab61f117600ee091
= <=>
2026-03-28 20:17:39 -0400

    add ScimUserController, update ScimExceptionHandler
    ScimUserController - POST, GET/{id}, GET list, PUT, DELETE

--- src/main/java/com/github/kushaal/scim_service/exception/ScimExceptionHandler.java
+++ src/main/java/com/github/kushaal/scim_service/exception/ScimExceptionHandler.java
@@ -3,12 +3,31 @@ package com.github.kushaal.scim_service.exception;
 import com.github.kushaal.scim_service.dto.response.ScimError;
 import com.github.kushaal.scim_service.model.ScimConstants;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ScimExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ScimError> handleValidation(MethodArgumentNotValidException ex) {
+        String detail = ex.getBindingResult().getFieldErrors().stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.parseMediaType(ScimConstants.SCIM_CONTENT_TYPE))
+                .body(ScimError.builder()
+                        .status(400)
+                        .scimType("invalidValue")
+                        .detail(detail)
+                        .build());
+    }
 
     @ExceptionHandler(ScimResourceNotFoundException.class)
     public ResponseEntity<ScimError> handleNotFound(ScimResourceNotFoundException ex) {
